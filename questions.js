d40fbf9c07669f923be381556bec30f3db0fcbfe
BibianaSanSao <bibiana.santos@live.com>
2022-04-12 09:51:19 +0100

    first com

---
+++ questions.js
@@ -0,0 +1,9 @@
+// Which city is the capital of Japan?
+// Kioto
+// Tokyo
+// Osaka
+
+// A tradicional Turkish breakfast, what does it consist of?
+// Pancakes, butter, jam, tea
+// Cheese, black olives, tomatoes, bread, butter, tea
+// Waffles, fruit, cheese, tea
